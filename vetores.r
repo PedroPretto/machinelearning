67d8ff7d56b9037ef8d4f58cf8d6914fdc9e35c8
PedroPretto <pedropretto18@gmail.com>
2019-09-09 21:01:12 -0300

    criando vetores

---
+++ vetores.r
@@ -0,0 +1,15 @@
+> colega = c("suy", "gui", "teles")
+> colega[1]
+[1] "suy"
+> colega[3]
+[1] "teles"
+> colega[1:2]
+[1] "suy" "gui"
+> colega[1:2]
+[1] "suy" "gui"
+> colega[-2]
+[1] "suy"   "teles"
+> colega[-1]
+[1] "gui"   "teles"
+> colega[-3]
+[1] "suy" "gui"
