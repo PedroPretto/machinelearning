924c81b1377baac583f1893bcf323d5d669c2a2a
PedroPretto <pedropretto18@gmail.com>
2019-09-09 21:18:57 -0300

    listas

---
+++ listas.r
@@ -0,0 +1,10 @@
+gender <- factor(c("MALE", "FEMALE", "MALE"))
+blood <- factor(c("O", "AB", "A"),
+                levels = c("A","B","AB","O"))
+gender
+blood
+
+subject1 <- list(fullname = colega[1], gender = gender[1], blood = blood[2])
+
+
+subject1
